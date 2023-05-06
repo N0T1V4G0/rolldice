5af0b0530cdbd3a129146b3f07b4083fc1fe49db
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-05-06 18:13:42 -0300

    feat: add dice_roller widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rolldice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
@@ -17,29 +16,8 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-6.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              onPressed: rollDice,
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
